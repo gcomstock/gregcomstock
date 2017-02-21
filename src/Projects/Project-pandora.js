0a0cd07914bf857cdd38fab5e376ac8e6ac0abf0
Greg Comstock <gcomstock@gmail.com>
2017-02-20 23:50:25 -0800

    cleanup

--- src/Projects/Project-pandora.js
+++ src/Projects/Project-pandora.js
@@ -8,15 +8,11 @@ export const PandoraLayout = React.createClass({
     return (
       <div className='ProjectView'>
         <Block3d cssClass='ProjectView__pic' width='100%' height='auto' depth='14' z='0'>
-          <img width='100%' src='work/pandora/pandora-hero-1.jpg' alt='portfolio'/>
+          <img width='100%' src='work/pandora/pandora-1.jpg' alt='portfolio'/>
         </Block3d>
 
         <Block3d cssClass='ProjectView__pic' width='100%' height='auto' depth='14' z='0'>
-          <img width='100%' src='work/pandora/pandora-hero-2.jpg' alt='portfolio'/>
-        </Block3d>
-
-        <Block3d cssClass='ProjectView__pic' width='100%' height='auto' depth='14' z='0'>
-          <img width='100%' src='work/pandora/pandora-hero-2.jpg' alt='portfolio'/>
+          <img width='100%' src='work/pandora/pandora-2.jpg' alt='portfolio'/>
         </Block3d>
       </div>
     );
--- src/Projects/Project-pandoraWin10.js
+++ src/Projects/Project-pandoraWin10.js
@@ -8,15 +8,15 @@ export const PandoraWin10Layout = React.createClass({
     return (
       <div className='ProjectView'>
         <Block3d cssClass='ProjectView__pic' width='100%' height='auto' depth='14' z='0'>
-          <img width='100%' src='work/pandora-windows10/pandora-hero-1.jpg' alt='portfolio'/>
+          <img width='100%' src='work/pandora-windows10/pandora-windows10-1.jpg' alt='portfolio'/>
         </Block3d>
 
         <Block3d cssClass='ProjectView__pic' width='100%' height='auto' depth='14' z='0'>
-          <img width='100%' src='work/pandora-windows10/pandora-hero-2.jpg' alt='portfolio'/>
+          <img width='100%' src='work/pandora-windows10/pandora-windows10-2.jpg' alt='portfolio'/>
         </Block3d>
 
         <Block3d cssClass='ProjectView__pic' width='100%' height='auto' depth='14' z='0'>
-          <img width='100%' src='work/pandora-windows10/pandora-hero-2.jpg' alt='portfolio'/>
+          <img width='100%' src='work/pandora-windows10/pandora-windows10-3.jpg' alt='portfolio'/>
         </Block3d>
       </div>
     );
@@ -27,20 +27,19 @@ export const PandoraWin10Layout = React.createClass({
 export const PandoraWin10Consts = Object.freeze({
   ROUTE: '/project/pandora-windows10',
   TITLE: 'pandora for windows 10',
-  SNIPPET: 'In preparation for on-demand listening, the new Pandora.com has been re-written from the ground up using the latest front-end technologies to address complex asynchronous use cases between audio playback and advertising.',
+  SNIPPET: 'Working with a small team, we leveraged the WinJS framework to create a touch-friendly desktop application running on javascript structured with backbone.js.',
   THUMBNAIL_PATH: 'work/pandora-windows10/tn.jpg',
   TEXT: [
-    'As a member of the web team, I develop production-ready, front-end code for several areas of pandora.com, with an emphasis on display ad integration, layout, and animation. We are actively working on bringing on-demand listening to the platform.'
+    'Working with a small team, we leveraged the WinJS framework to create a touch-friendly desktop application running on javascript structured with backbone.js. I coordinated closely with product design to ensure touch events on the Microsoft Surface were responsive and the animation was smooth. (better screen shots coming soon)'
   ],
   RESPONSIBILITIES: [
-    'Collaborate closely with design and product on core UI decisions',
-    'Development of react components for main application views',
-    'Display ad integration',
+    'Core UI decision-making with product design team',
+    'Development of backbone views, including main screen',
     'UI animations'
   ],
   TECHNOLOGIES: [
-    'React, Redux, ES6',
-    'Webpack, npm, CSS/SASS',
-    'Photoshop, Illustrator'
+    'Javascript, backbone, WinJS',
+    'Moushtache templating',
+    'CSS/SASS'
   ]
 });
