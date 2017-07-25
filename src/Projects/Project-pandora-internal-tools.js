b631d20b913da902ba65b4f6c42766c789bc3f50
Greg Comstock <gcomstock@pandora.com>
2017-07-25 15:48:40 -0700

    update

--- src/Projects/Project-pandora-internal-tools.js
+++ src/Projects/Project-pandora-internal-tools.js
@@ -8,32 +8,32 @@ export const Content = Object.freeze({
   ROUTE: '/project/pandora-internal-tools',
   EXTERNAL_URL: '',
 
-  PREVIEW_TITLE: 'Pandora Internal Tools',
-  PREVIEW_SNIPPET: 'When working on a large, public-facing product, design iteration usually requires scrutinizing over analytics, focus group sessions, and sometimes just plain intuition. By contrast, internal tools can be a delight to work on, because you have the luxury of meeting directly with your users and understanding exactly what they need! At Pandora, I’ve had the pleasure of contributing to several internal tools and design processes. Seeing your coworkers light up when presenting or implementing new designs to make their work easier is deeply rewarding to me.',
-  PREVIEW_THUMBNAILPATH: 'work/pandora-internal-tools/tn.jpg',
+  PREVIEW_TITLE: 'Pandora Internal Tools & Sites',
+  PREVIEW_SNIPPET: 'At Pandora, I’ve had the opportunity to design and develop both internal tools and external sites for different internal organizations. The challenges are different from working on a company’s flagship product, but the sense of ownership and satisfaction of making your co-worker’s lives easier can be more fulfilling.',
+  PREVIEW_THUMBNAILPATH: 'work/pandora-internal-tools/tn.png',
 
-  TITLE: 'Pandora Internal Tools',
+  TITLE: 'Pandora Internal Tools & Sites',
 
   NUGGET1__SUBHEAD: 'Synopsis',
-  NUGGET1__TEXT: 'When working on a large, public-facing product, design iteration usually requires scrutinizing over analytics, focus group sessions, and sometimes just plain intuition. By contrast, internal tools can be a delight to work on, because you have the luxury of meeting directly with your users and understanding exactly what they need! At Pandora, I’ve had the pleasure of contributing to several internal tools and design processes. Seeing your coworkers light up when presenting or implementing new designs to make their work easier is deeply rewarding to me.',
-  //NUGGET2__SUBHEAD: 'Role',
-  //NUGGET2__TEXT: '<b>Product Designer</b> - Ad layout, display animation, customization options<br/><b>Front-end Engineer</b> - Pandora.com integration, tool development<br/>',
-  //NUGGET3__SUBHEAD: 'Technologies',
-  //NUGGET3__TEXT: 'React + Redux (Javascript ES6)<br/>Webpack, npm, SASS<br/>Photoshop<br/>Sketch',
+  NUGGET1__TEXT: 'When working on a large, public-facing product, design iteration usually requires scrutinizing over analytics, focus group sessions, A/B testing, and sometimes just plain intuition. By contrast, internal tools can be a delight to work on, because you have the luxury of meeting directly with your users and understanding exactly what they need! At Pandora, I’ve had the pleasure of contributing to several internal tools, as well as public-facing sites for internal organizations. Seeing your co-workers light up when presenting or implementing new designs to make their work easier is deeply rewarding.',
 
+  ICONS__SUBHEAD: 'Rhythm Icon Library',
+  ICONS__DESC: 'This video walks through an internal tool I designed and developed for Pandora, to ensure that we have a single, evolving source of truth for all of our company’s iconography. It is searchable, and uses a backend script that calls our cloud storage API to keep the design team’s icon folder in sync with the icons surfaced on this page, allowing several designers to contribute to and evolve this library without having to touch any code.',
+  ICONS__VIDEOPATH: 'https://player.vimeo.com/video/226627838',
+  ICONS__WEBVIDEORATIO: 39.2,
+  ICONS__MOBILEVIDEORATIO: 60,
 
-  ADSKIN_BUILDER__SUBHEAD: 'Pandora Icon Library',
-  ADSKIN_BUILDER__DESC: 'With freedom comes responsibility! Because Pandora\'s new display ads have the ability to do so much with the pandora.com UI, there’s an inherent possibility for human error, or even exploitation from an advertiser. To solve this, I designed and developed an internal tool that Pandora\'s in-house ad design team uses to generate the CSS file for a corresponding display ad. The predefined form fields and live preview ensures visual consistency with no syntax errors.',
-  ADSKIN_BUILDER__VIDEOPATH: 'https://player.vimeo.com/video/226793914',
-  ADSKIN_BUILDER__WEBVIDEORATIO: 39.2,
-  ADSKIN_BUILDER__MOBILEVIDEORATIO: 60,
-
-
-  AD_OPTIMIZATIONS__SUBHEAD: 'Optimizing the<br/>Ad Impression',
-  AD_OPTIMIZATIONS__DESC: 'While implementing display ads on the new Pandora.com, I recognized an important optimization: By toggling display ad impressions between two alternating iframes, we have the ability to invisibly preload the next display ad while the current one is still active. Once the next display ad iframe has fired its onload event, we can perform an elegant cross-fade between ads, and remove the old one from the DOM once its fadeout animation ends. This design decision ensures a display ad is always visible, because no impression time is spent waiting for ad assets to load. The result has been about a 15% increase in display ad capacity without impacting the user experience. This video highlights the transition between display ads as the user interacts with the site.',
-  AD_OPTIMIZATIONS__VIDEOPATH: 'https://player.vimeo.com/video/226793914',
-  AD_OPTIMIZATIONS__WEBVIDEORATIO: 39.2,
-  AD_OPTIMIZATIONS__MOBILEVIDEORATIO: 60
+  CAREERS__SUBHEAD: 'Pandora Careers',
+  CAREERS__DESC: 'Working with our employee experience team, I designed and developed our original careers page to provide a look into Pandora’s employee culture and job opportunities. Using Jobvite’s API, the job postings stay up to date and are sortable through javascript that I authored. Since its launch in 2013, this page has evolved and improved under fellow product design Joyce Lau, however the original design I created can be seen in this video.',
+  CAREERS__VIDEOPATH: 'https://player.vimeo.com/video/226922020',
+  CAREERS__WEBVIDEORATIO: 39.2,
+  CAREERS__MOBILEVIDEORATIO: 60,
+  
+  INSIGHTS__SUBHEAD: 'Campaign Insights',
+  INSIGHTS__DESC: 'One of Pandora’s premium advertising products are custom, sponsored radio stations with audio content that can only be heard on Pandora. I collaborated with fellow designer Savio Alphonso to create an internal dashboard that our analysts and campaign managers can use to evaluate the success of these stations. We both worked to develop a clean way to present information graphics, and then I introduced interaction animation and UI work into the codebase alongside full-stack engineers. This was my first exposure to version control, code review, and pair programming, so it holds a special place in my heart!',
+  INSIGHTS__VIDEOPATH: 'https://player.vimeo.com/video/226934506',
+  INSIGHTS__WEBVIDEORATIO: 39.2,
+  INSIGHTS__MOBILEVIDEORATIO: 60
 });
 
 
@@ -75,7 +75,7 @@ export const Layout = React.createClass({
             noRight={true}
             isMobile={this.props.isMobile}
           >
-            <img width='100%' src='work/pandora-internal-tools/pandora-internal-tools-1.jpg' alt='portfolio'/>
+            <img width='100%' src='work/pandora-internal-tools/pandora-internal-tools-1.png' alt='portfolio'/>
           </Block3d>
         </div>
 
@@ -89,23 +89,34 @@ export const Layout = React.createClass({
 
 
         <ProjectGroup
-          subhead={Content.ADSKIN_BUILDER__SUBHEAD}
-          desc={Content.ADSKIN_BUILDER__DESC}
+          subhead={Content.CAREERS__SUBHEAD}
+          desc={Content.CAREERS__DESC}
           isVideo={true}
-          videoPath={Content.ADSKIN_BUILDER__VIDEOPATH}
-          webVideoRatio={Content.ADSKIN_BUILDER__WEBVIDEORATIO}
-          mobileVideoRatio={Content.ADSKIN_BUILDER__MOBILEVIDEORATIO}
+          videoPath={Content.CAREERS__VIDEOPATH}
+          webVideoRatio={Content.CAREERS__WEBVIDEORATIO}
+          mobileVideoRatio={Content.CAREERS__MOBILEVIDEORATIO}
           isMobile={this.props.isMobile}
         />
 
 
         <ProjectGroup
-          subhead={Content.AD_OPTIMIZATIONS__SUBHEAD}
-          desc={Content.AD_OPTIMIZATIONS__DESC}
+          subhead={Content.INSIGHTS__SUBHEAD}
+          desc={Content.INSIGHTS__DESC}
           isVideo={true}
-          videoPath={Content.AD_OPTIMIZATIONS__VIDEOPATH}
-          webVideoRatio={Content.AD_OPTIMIZATIONS__WEBVIDEORATIO}
-          mobileVideoRatio={Content.AD_OPTIMIZATIONS__MOBILEVIDEORATIO}
+          videoPath={Content.INSIGHTS__VIDEOPATH}
+          webVideoRatio={Content.INSIGHTS__WEBVIDEORATIO}
+          mobileVideoRatio={Content.INSIGHTS__MOBILEVIDEORATIO}
+          isMobile={this.props.isMobile}
+        />
+
+
+        <ProjectGroup
+          subhead={Content.ICONS__SUBHEAD}
+          desc={Content.ICONS__DESC}
+          isVideo={true}
+          videoPath={Content.ICONS__VIDEOPATH}
+          webVideoRatio={Content.ICONS__WEBVIDEORATIO}
+          mobileVideoRatio={Content.ICONS__MOBILEVIDEORATIO}
           isMobile={this.props.isMobile}
         />
 
